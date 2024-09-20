0c85c42353b8952a979a1ca394ffb7ebb667a824
rondinelli-oliveira <rondinelliads@gmail.com>
2024-09-20 17:11:00 -0300

    Implementando content negotiation para retornar JSON ou XML

--- src/main/java/com/evolution/food/api/controller/KitchenController.java
+++ src/main/java/com/evolution/food/api/controller/KitchenController.java
@@ -3,10 +3,12 @@ package com.evolution.food.api.controller;
 import com.evolution.food.api.domain.model.Kitchen;
 import com.evolution.food.api.domain.repository.KitchenRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
 import java.util.List;
 
 @RestController
@@ -20,11 +22,22 @@ public class KitchenController {
         this.kitchenRepository = kitchenRepository;
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Kitchen> findAll() {
     	List<Kitchen> kitchens = kitchenRepository.findAll();
     	for(Kitchen kitchen: kitchens) {
             log.info("Nome da cozinha de codigo: {} e {}", kitchen.getId(), kitchen.getName());
+            log.info("Content application/json");
+    	}
+        return kitchenRepository.findAll();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public List<Kitchen> findAll1() {
+    	List<Kitchen> kitchens = kitchenRepository.findAll();
+    	for(Kitchen kitchen: kitchens) {
+            log.info("Nome da cozinha de codigo: {} e {}", kitchen.getId(), kitchen.getName());
+            log.info("Content application/xml");
     	}
         return kitchenRepository.findAll();
     }
