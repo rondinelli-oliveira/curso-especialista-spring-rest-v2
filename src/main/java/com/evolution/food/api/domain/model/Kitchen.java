6283439958918a6a8a22e76cb017200583e4e809
rondinelli-oliveira <rondinelliads@gmail.com>
2024-09-20 09:31:16 -0300

    Adicionando lombok

--- src/main/java/com/evolution/food/api/domain/model/Kitchen.java
+++ src/main/java/com/evolution/food/api/domain/model/Kitchen.java
@@ -1,47 +1,22 @@
 package com.evolution.food.api.domain.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.util.Objects;
-
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Kitchen {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null || getClass() != obj.getClass())
-            return false;
-
-        Kitchen kitchen = (Kitchen) obj;
-        return Objects.equals(id, kitchen.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
--- src/main/java/com/evolution/food/api/domain/model/Restaurant.java
+++ src/main/java/com/evolution/food/api/domain/model/Restaurant.java
@@ -1,13 +1,17 @@
 package com.evolution.food.api.domain.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurant {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,43 +21,4 @@ public class Restaurant {
     @Column(name = "freigth_rate")
     private BigDecimal freightRate;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getFreightRate() {
-        return freightRate;
-    }
-
-    public void setFreightRate(BigDecimal freightRate) {
-        this.freightRate = freightRate;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null || getClass() != obj.getClass())
-            return false;
-
-        Restaurant that = (Restaurant) obj;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 }
