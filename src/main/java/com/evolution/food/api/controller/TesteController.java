aaf2d40946d70dcf49c7afad9e00f190bb3db885
rondinelli-oliveira <rondinelliads@gmail.com>
2024-09-25 13:01:52 -0300

    Criando consultas com query methods

--- src/main/java/com/evolution/food/api/controller/TesteController.java
+++ src/main/java/com/evolution/food/api/controller/TesteController.java
@@ -5,6 +5,7 @@ import com.evolution.food.api.domain.repository.KitchenRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teste")
@@ -16,8 +17,13 @@ public class TesteController {
         this.kitchenRepository = kitchenRepository;
     }
 
-//    @GetMapping("/kitchens/by-name")
-//    public List<Kitchen> kitchensByName(@RequestParam("name") String name) {
-//        return kitchenRepository.findByName(name);
-//    }
+    @GetMapping("/kitchens/all-by-name")
+    public List<Kitchen> kitchensByName(@RequestParam("name") String name) {
+        return kitchenRepository.findAllByName(name);
+    }
+
+    @GetMapping("/kitchens/by-name")
+    public Optional<Kitchen> kitchenByName(@RequestParam("name") String name) {
+        return kitchenRepository.findByName(name);
+    }
 }
--- src/main/java/com/evolution/food/api/domain/repository/KitchenRepository.java
+++ src/main/java/com/evolution/food/api/domain/repository/KitchenRepository.java
@@ -4,9 +4,14 @@ import com.evolution.food.api.domain.model.Kitchen;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface KitchenRepository extends JpaRepository<Kitchen, Long> {
 
-//	List<Kitchen> findByName(String name);
+	List<Kitchen> findAllByName(String name);
+
+    Optional<Kitchen> findByName(String name);
 
 }
