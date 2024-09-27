cf658f4724636170c4170ccddc99d90ef40a5a99
rondinelli-oliveira <rondinelliads@gmail.com>
2024-09-27 13:06:21 -0300

    Mapeando relacionamento muitos-para-um produtos e restaurante

---
+++ src/main/java/com/evolution/food/api/domain/model/Product.java
@@ -0,0 +1,34 @@
+package com.evolution.food.api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Product {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurant restaurant;
+}
