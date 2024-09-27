bc912f98b1e2b0b69112973443d064175520b24a
rondinelli-oliveira <rondinelliads@gmail.com>
2024-09-27 09:17:40 -0300

    Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/evolution/food/api/domain/model/Kitchen.java
+++ src/main/java/com/evolution/food/api/domain/model/Kitchen.java
@@ -1,9 +1,13 @@
 package com.evolution.food.api.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @JsonRootName("cozinha")
 @Data
@@ -18,5 +22,9 @@ public class Kitchen {
 
     @Column(nullable = false)
     private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "kitchen")
+    List<Restaurant> restaurants = new ArrayList<>();
 
 }
